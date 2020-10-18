e9458caf8f725bc51d1cf9c64db4b7e3f4fcc046
Glitch (sincere-vaulted-modem) <none>
2020-10-18 19:15:24 +0000

    🏕💪 Checkpoint ./commands/4k.js:20882587/20

--- commands/4k.js
+++ commands/4k.js
@@ -9,7 +9,7 @@ if(message.channel.nsfw == false) return message.channel.send(new Discord.Messag
 if(await data.fetch(`slm.${message.author.id}.${message.guild.id}.4k`)) return message.channel.send(new Discord.MessageEmbed().setColor('#00001').setDescription(`**10 saniyenin altında hızlı kullanamazsın!**`));
 DabiClient.nsfw.real.random().then(json => {
 message.channel.send(new Discord.MessageAttachment(json.url));
-data.set(`slm.${message.author.id}.${message.guild.id}.4k`, 'codare');
+data.set(`slm.${message.author.id}.${message.guild.id}.4k`, 'Gweep Creative');
 setTimeout(() => {
 data.delete(`slm.${message.author.id}.${message.guild.id}.4k`)
 }, 10000);
