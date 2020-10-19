f33b8a048bce05a574ce9a12c3d655bd8e0f389a
Glitch (clooudup) <none>
2020-10-19 08:28:57 +0000

    🚝🛬 Checkpoint ./events/ready.js:21004775/1790 ./util/eventLoader.js:21004775/324

---
+++ events/ready.js
@@ -0,0 +1,16 @@
+const chalk = require("chalk");
+const moment = require("moment");
+const Discord = require("discord.js");
+const rabel = require('../gweep/rabel.json');
+var prefix = rabel.prefix;
+
+module.exports = async client => {
+
+    client.user.setActivity(``, { type: "PLAYING" });
+  client.user.setStatus("online");
+  console.log("Aktif!")
+};
+
+  //LISTENING = DİNLİYOR
+  //WATCHING = İZLİYOR
+  //PLAYING = OYNUYOR 
